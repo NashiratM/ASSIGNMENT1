03df28aaedddf3013bc9c884fa3f70ead22b0540
NashiratM <52419877+NashiratM@users.noreply.github.com>
2021-06-09 18:31:11 +0100

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,10 @@
+#Madlibs game
+verb1 = input ('enter a verb in the past tense:')
+verb2 = input ("enter a verb in the past tense:")
+feeling = input ("enter a feeling towards someone :")
+verb3 = input ("Enter a verb ending in ing :")
+adjective = input ("Enter an adjective")
+print ("Roses are " + adjective)
+print (noun1 + " is blue")
+print ("i can't start my day without " , noun2)
+
